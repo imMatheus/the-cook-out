607f9e155dc28333fe7a906f69f51233b947dafe
imMatheus <matheusmendesbarata03@gmail.com>
2023-04-02 17:04:26 +0200

    added start code for planetscale

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module github.com/imMatheus/the-cook-out
 
 go 1.19
+
+require github.com/go-sql-driver/mysql v1.7.0 // indirect
--- server/main.go
+++ server/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
-	fmt.Println("hej")
+	db, err := sql.Open("mysql", os.Getenv("DSN"))
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("failed to ping: %v", err)
+	}
+
+	log.Println("Successfully connected to PlanetScale!")
 }
